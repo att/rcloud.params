504ce90b3a3eefb155ef9a5657a6905701d9956c
Gordon Woodhull <gordon@woodhull.com>
2015-10-18 02:14:22 -0400

    set focus on first empty field

--- inst/javascript/rcloud.params.js
+++ inst/javascript/rcloud.params.js
@@ -43,10 +43,14 @@
     function input_id(name) {
         return 'rcloud-params-' + name;
     }
+    function have_value(name) {
+        return _varmap[name] != undefined ||
+            _defaults[name] !== undefined;
+    }
     var result = {
         init: function(k) {
             _varmap = querystring.parse();
-            k(_varmap);
+            k(null, _varmap);
         },
         set_query: function(key, value, k) {
             if(value !== undefined && _defaults[key] !== value)
@@ -54,10 +58,16 @@
             else
                 delete _varmap[key];
             querystring.update(_varmap);
-            k(null, 1);
+            if(k)
+                k(null, 1);
+        },
+        focus: function(name, k) {
+            $('#' + input_id(name)).focus();
+            if(k)
+                k(null, 1);
         },
         add_edit_control: function(context_id, desc, name, def, callback, k) {
-            var input = $('<input type="text" id="#' + input_id(name) + '"></input>');
+            var input = $('<input type="text" id="' + input_id(name) + '"></input>');
             var label = $('<label>' + desc + '</label>').append(input);
             if(def !== null) {
                 _defaults[name] = def;
@@ -66,7 +76,7 @@
             input.change(function() {
                 var val = input.val().trim();
                 if(val === '') val = undefined;
-                result.set_query(name, val, function() {});
+                result.set_query(name);
             });
             RCloud.session.selection_out(context_id, label);
             _needed.push(name);
@@ -75,17 +85,16 @@
         wait_submit: function(context_id, k) {
             var submit = $('<input type="button" value="Submit" />');
             submit.click(function() {
-                if(_needed.every(function(n) {
-                    return _varmap[n] != undefined ||
-                        _defaults[n] !== undefined;
-                }))
+                if(_needed.every(have_value))
                     k(_.pick(_varmap, _needed));
+                else {
+                    var first_empty = _.find(_needed, function(n) {
+                        return !have_value(n);
+                    });
+                    result.focus(first_empty);
+                }
             });
             RCloud.session.selection_out(context_id, submit);
-        },
-        focus: function(name, k) {
-            $('#' + input_id(name)).focus();
-            k(null, 1);
         }
     };
     return result;
