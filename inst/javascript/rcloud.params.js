c2a91662501dcf00a1c087b3dd7b35c5ab7b1cc2
Beth Ashlee <bashlee@mango-solutions.com>
2018-03-28 12:05:18 +0100

    Tidy up js

--- inst/javascript/rcloud.params.js
+++ inst/javascript/rcloud.params.js
@@ -1,25 +1,14 @@
-((function() {
-
-    // leave require empty for now - ui control libraries will go here
-    requirejs.config({
-        "paths": {
-        },
-        "shim": {
-        }
-    });
-    
-
+((function () {
     // stolen from dc.graph.js, could be made its own tiny library
     // Used to retrive and update url
-    var querystring = (function() {
+    var querystring = (function () {
         return {
-            parse: function() {
-                return (function(a) {
+            parse: function () {
+                return (function (a) {
                     if (a == "") return {};
                     var b = {};
-                    for (var i = 0; i < a.length; ++i)
-                    {
-                        var p=a[i].split('=', 2);
+                    for (var i = 0; i < a.length; ++i) {
+                        var p = a[i].split('=', 2);
                         if (p.length == 1)
                             b[p[0]] = "";
                         else
@@ -28,10 +17,10 @@
                     return b;
                 })(window.location.search.substr(1).split('&'));
             },
-            update: function(m) {
+            update: function (m) {
                 var base = window.location.protocol + '//' + window.location.host + window.location.pathname;
                 var parts = [];
-                for(var k in m)
+                for (var k in m)
                     parts.push(k + '=' + encodeURIComponent(m[k]));
                 var url = base + '?' + parts.join('&');
                 window.history.pushState(null, null, url);
@@ -49,140 +38,139 @@
         return _varmap[name] != undefined ||
             _defaults[name] !== undefined;
     }
-    function get_input_value(label){ // takes jquery object and extracts value
-     
-              if(label[0].childNodes[1].nodeName.toLowerCase() == 'select'){
-                // if a select get all selected objects
-                return $('#' + label[0].childNodes[1].id + ' option:selected').map(function() {return $(this).val();}).get();
-              } else{
-                return label[0].querySelector("[id^='rcloud-params-']").value.trim();
-                
-              }
-    }
-  // Used to combine value with class to push back to R  
-  combine = (obj1, obj2) => {
-	let returnObject = {},
-  		objects = _.chain(_.map(Object.keys(obj1), (key) => {
-  	return [obj1[key], obj2[key]];
-  })).map((items, index) => {
-  	return {
-    	value: items[0],
-      type: items[1]
-    };
-  }).value();
+    function get_input_value(label) { // takes jquery object and extracts value
 
-	_.each(Object.keys(obj1), (key, index) => {
-  	returnObject[key] = objects[index];
-  });
- 
- 	return returnObject;
-};
+        if (label[0].childNodes[1].nodeName.toLowerCase() == 'select') {
+            // if a select get all selected objects
+            return $('#' + label[0].childNodes[1].id + ' option:selected').map(function () { return $(this).val(); }).get();
+        } else {
+            return label[0].querySelector("[id^='rcloud-params-']").value.trim();
+
+        }
+    }
+    // Used to combine value with class to push back to R  
+    combine = (obj1, obj2) => {
+        let returnObject = {},
+            objects = _.chain(_.map(Object.keys(obj1), (key) => {
+                return [obj1[key], obj2[key]];
+            })).map((items, index) => {
+                return {
+                    value: items[0],
+                    type: items[1]
+                };
+            }).value();
+
+        _.each(Object.keys(obj1), (key, index) => {
+            returnObject[key] = objects[index];
+        });
+
+        return returnObject;
+    };
     var result = {
-        init: function(k) {
+        init: function (k) {
             _varmap = querystring.parse();
             k(null, _varmap);
         },
-        set_query: function(key, value, varClass, k) {
-            if(value !== undefined && _defaults[key] !== value) {
+        set_query: function (key, value, varClass, k) {
+            if (value !== undefined && _defaults[key] !== value) {
                 _varmap[key] = value;
                 _varClass[key] = varClass;
             }
-              else
+            else
                 delete _varmap[key];
             querystring.update(_varmap);
-            if(k)
+            if (k)
                 k(null, 1);
         },
-        focus: function(name, k) {
+        focus: function (name, k) {
             $('#' + input_id(name)).focus();
-            if(k)
+            if (k)
                 k(null, 1);
         },
         error_highlight(names, whether, k) {
-            if(!_.isArray(names))
+            if (!_.isArray(names))
                 names = [names];
-            var sel = $(names.map(function(n) { return '#' + input_id(n); }).join(',')),
+            var sel = $(names.map(function (n) { return '#' + input_id(n); }).join(',')),
                 border = whether ? 'red 1px solid' : '';
             sel.css('border', border);
-            if(k)
+            if (k)
                 k(null, 1);
-        },     
- // copied over from rcloud.web - need to be moved back to caps.R       
-        appendDiv: function(div, content, k) {
-          if (_.isFunction(content)) content = content();
-          $(div).append(content);
-          k(true);
         },
-      appendElement: function(div, content, k) {
-        if (_.isFunction(content)) content = content();
-        $(div).append($('#' + content));
-        k(true);
-      },
-      prependDiv: function(div, content, k) {
-        if (_.isFunction(content)) content = content();
-        $(div).prepend(content);
-        k(true);
-      },
-      prependElement: function(div, content, k) {
-        if (_.isFunction(content)) content = content();
-        $(div).prepend($('#' + content));
-        k(true);
-      },
-      setDiv: function(div, content, k) {
-        if (_.isFunction(content)) content = content();
-        $(div).empty(content);
-        $(div).append(content);
-        k(true);
-      },
-      setElement: function(div, content, k) {
-        if (_.isFunction(content)) content = content();
-        $(div).empty($('#' + content));
-        $(div).append($('#' + content));
-        k(true);
-      },
-  
-        add_edit_control: function(context_id, desc, name, def, val, inputTag, labelTag, varClass, callback, k) {
-   
+        // copied over from rcloud.web - need to be moved back to caps.R       
+        appendDiv: function (div, content, k) {
+            if (_.isFunction(content)) content = content();
+            $(div).append(content);
+            k(true);
+        },
+        appendElement: function (div, content, k) {
+            if (_.isFunction(content)) content = content();
+            $(div).append($('#' + content));
+            k(true);
+        },
+        prependDiv: function (div, content, k) {
+            if (_.isFunction(content)) content = content();
+            $(div).prepend(content);
+            k(true);
+        },
+        prependElement: function (div, content, k) {
+            if (_.isFunction(content)) content = content();
+            $(div).prepend($('#' + content));
+            k(true);
+        },
+        setDiv: function (div, content, k) {
+            if (_.isFunction(content)) content = content();
+            $(div).empty(content);
+            $(div).append(content);
+            k(true);
+        },
+        setElement: function (div, content, k) {
+            if (_.isFunction(content)) content = content();
+            $(div).empty($('#' + content));
+            $(div).append($('#' + content));
+            k(true);
+        },
+
+        add_edit_control: function (context_id, desc, name, def, val, inputTag, labelTag, varClass, callback, k) {
+
             var input = $(inputTag);
             var label = $(labelTag).append(input);
-            if(val !== null) {
-                _varmap[name] = val ;
+            if (val !== null) {
+                _varmap[name] = val;
                 _varClass[name] = varClass;
                 input.val(val);
-                result.set_query(name, val, varClass); 
-            }
-            else if(def !== null) {
-                _defaults[name] = def; 
-                 _varClass[name] = varClass;
+                result.set_query(name, val, varClass);
+            } else if (def !== null) {
+                _defaults[name] = def;
+                _varClass[name] = varClass;
                 input.val(def);
-                result.set_query(name, val, varClass); 
+                result.set_query(name, val, varClass);
+            } else if (_varmap[name] !== null) {
+                input.val(_varmap[name]);
             }
-            label.on('change', function() {
-           
+            label.on('change', function () {
+
                 var val = get_input_value(label);
-                
-                if(val === '') val = undefined;
-                result.set_query(name, val, varClass);      
+
+                if (val === '') val = undefined;
+                result.set_query(name, val, varClass);
             });
-            RCloud.session.invoke_context_callback('selection_out', context_id, label); 
+            RCloud.session.invoke_context_callback('selection_out', context_id, label);
             _needed.push(name);
 
-          k(null, label.attr('id'));
-           //k(null, label.html());
+            k(null, label.attr('id'));      
         },
-        wait_submit: function(context_id, k) {
+        wait_submit: function (context_id, k) {
             var submit = $('<input id = "rcloud-params-submit" type="button" value="Submit" />');
-            submit.click(function() {
-             
+            submit.click(function () {
+
                 var good_bad = _.partition(_needed, have_value);
-               
+
                 result.error_highlight(good_bad[0], false);
-                if(!good_bad[1].length) {
+                if (!good_bad[1].length) {
                     submit.attr('disabled', 'disabled');
-                  var varValues = _.pick(_varmap, _needed);
-                  k(combine(varValues, _varClass));
-                }
-                else {
+                    var varValues = _.pick(_varmap, _needed);
+                    k(combine(varValues, _varClass));
+                } else {
                     result.error_highlight(good_bad[1], true);
                     result.focus(good_bad[1][0]);
                 }
