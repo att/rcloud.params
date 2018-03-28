21fc10418bf07382913cdf8fb2cfc4a86d47759f
Beth Ashlee <bashlee@mango-solutions.com>
2018-03-28 09:08:12 +0100

    Tidy comments

--- R/zzz.R
+++ R/zzz.R
@@ -7,13 +7,13 @@ input.QS <- NULL
   f <- function(module.name, module.path) {
     path <- system.file("javascript", module.path, package="rcloud.params")
     caps <- rcloud.install.js.module(module.name,
-                                     paste(readLines(path), collapse='\n'))  # Install javascript files on browser-side
+                                     paste(readLines(path), collapse='\n')) 
     caps
   }
-  input.caps <<- f("rcloud.params", "rcloud.params.js")  # is everything on js side eg init, focus, error_highlight, submit_wait() 
+  input.caps <<- f("rcloud.params", "rcloud.params.js") 
   
   if(!is.null(input.caps)) {
-    # init calls js queryString which grabs url and splits. init also has a method for updating url
+    # init calls js queryString which grabs url and splits. init also has update method 
     input.QS <<- input.caps$init()  # $notebook in list with notebook id string
     
   }
